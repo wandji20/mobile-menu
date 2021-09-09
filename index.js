3b89a3c1182cd1d74ac79a832263959d62602a14
wandji20 <wbertrand10@gmail.com>
2021-09-09 12:24:10 +0100

    Add method to toggle nav on mobile screen

---
+++ index.js
@@ -0,0 +1,26 @@
+const header = document.querySelector('header');
+const burgerButton = document.querySelector('.menu-bars');
+const burgerIcon = '<i class="fas fa-bars"></i>';
+const nav = document.getElementById('desktop-nav');
+console.log(header);
+// console.log(burgerButton);
+// console.log(burgerIcon);
+// console.log(nav);
+
+const showNav = () => {
+  if (nav.style.display === '') {
+    burgerButton.innerHTML = 'X';
+    burgerButton.classList.add('active-button');
+    nav.style.display = 'flex';
+    nav.classList.add('active');
+  } else {
+    nav.classList.remove('active');
+    burgerButton.classList.remove('active-button');
+    burgerButton.innerHTML = burgerIcon;
+    nav.style.display = '';
+  }
+};
+
+burgerButton.addEventListener('click', () => {
+  showNav();
+});
