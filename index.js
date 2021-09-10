c0d15bfc981faa002c6fccc6e02890e72897429e
wandji20 <wbertrand10@gmail.com>
2021-09-10 08:50:39 +0100

    Modify showNav function to change burger to and "X" and reverse on click

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const header = document.querySelector('header');
 const burgerButton = document.querySelector('.menu-bars');
 const nav = document.getElementById('desktop-nav');
-console.log(header);
-// const burgerIcon = '<i class="fas fa-bars"></i>';
-// console.log(burgerButton);
-// console.log(burgerIcon);
-// console.log(nav);
+
+const burgerIcon = '<i class="fas fa-bars"></i>';
 
 const showNav = () => {
   if (nav.style.display === '') {
     burgerButton.classList.add('active-button');
+    burgerButton.innerHTML = 'X'
     nav.style.display = 'flex';
     nav.classList.add('active');
   } else {
+    burgerButton.innerHTML = burgerIcon;
     nav.classList.remove('active');
     burgerButton.classList.remove('active-button');
     nav.style.display = '';
